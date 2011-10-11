ebbecb2e5c68f71324c0f3e7f467bf33c4f60d92
Mikko Koponen <mikko.koponen@reaktor.fi>
2011-10-11 14:05:21 +0300

    Improved datatype handling, added generic handler, fix ajax.cache = false.
    * Use datatype declared in global ajax options if no specific datatype given
    * Register an ajax handler for all requests with smoax.register(response)
    * Handle ajaxOpts.cache = false adding a random param in urls

--- jasmine-smoax.js
+++ jasmine-smoax.js
@@ -22,7 +22,7 @@ function Smoax() {
         if (abort) return;
         me.latest = options;
         me.calls.set(options.type, options.url, options);
-        var handler = me.handlers.get(options.type, options.url);          
+        var handler = me.handlers.get(options.type, options.url);
         if (!!handler) {
           var responses = { text:toText(handler.response) }; 
           //, xml:jQuery.parseXML(handler.response) };
@@ -65,7 +65,8 @@ function Smoax() {
   }
 
   function wrap(opts) {
-    opts.originalDataType = opts.dataType;
+    var allOpts = jQuery.ajaxSetup({}, opts)
+    opts.originalDataType = allOpts.dataType;
     opts.dataType = 'mock';
     return me.ajax(opts);
   }
@@ -75,14 +76,17 @@ function Smoax() {
     return this;
   }
   this.register = function(method, url, response) {
+    if (!url) { response = method; method = url = '*'; }
     var data = { statusCode:200, statusText:'success', response:response };
     return _register(method, url, data);
   }
   this.registerAsync = function(method, url, response) {
+    if (!url) { response = method; method = url = '*'; }
     var data = { statusCode:200, statusText:'success', response:response, async:true };
     return _register(method, url, data);
   }
   this.registerError = function(method, url, statusCode, statusText, response) {
+    if (!statusText) { response = statusCode; statusText = url; statusCode = method; method = url = '*'; }
     var data = { statusCode:statusCode, statusText:statusText, response:response };
     return _register(method, url, data);
   }
@@ -135,7 +139,7 @@ function Smoax() {
   function match(that, opts, method, url, data) {
     var requestMatch =  !!opts && !!opts.type
       && method.toUpperCase() == opts.type.toUpperCase()
-      && url == opts.url;
+      && url == opts.url.replace(/\?_=\d+/, '');
    var expectedData = toParams(data);
    var dataMatch = !!opts && expectedData == opts.data;
    if (!requestMatch) {
@@ -147,7 +151,7 @@ function Smoax() {
   }
 
   function key(method, url) {
-    return method.toUpperCase()+' '+url;
+    return method.toUpperCase()+' '+url.replace(/\?_=\d+/, '');
   }
   
   function AjaxMap() {
@@ -155,7 +159,7 @@ function Smoax() {
     this.map = {};
 
     this.get = function(method, url) {
-      return this.map[key(method, url)];
+      return this.map[key(method, url)] || this.map['* *'];
     };
     this.set = function(method, url, data) {
       var k = key(method, url);
