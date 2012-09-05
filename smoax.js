432eaff4f4893f72a10bba5444951cb706ca312e
Mikko Koponen <mikko.koponen@reaktor.fi>
2012-09-05 19:28:44 +0300

    Remove useless ;'s

--- smoax.js
+++ smoax.js
@@ -1,98 +1,98 @@
-var smoax = new Smoax();
+var smoax = new Smoax()
 
 function Smoax() {
-  var me = this;
-  this.ajax = $.ajax;
-  $.ajaxTransport('mock', mockAjaxTransport);
+  var me = this
+  this.ajax = $.ajax
+  $.ajaxTransport('mock', mockAjaxTransport)
 
   this.setup = function() {
-    this.latest = undefined;
-    this.handlers = new AjaxMap();
-    this.calls = new AjaxMap();
-    spyOn($, 'ajax').andCallFake(wrap);
-    return this.matchers;
+    this.latest = undefined
+    this.handlers = new AjaxMap()
+    this.calls = new AjaxMap()
+    spyOn($, 'ajax').andCallFake(wrap)
+    return this.matchers
   }
 
   function mockAjaxTransport(options, originalOptions, jqXHR) {
-    var abort = false;
-    options.dataType = originalOptions.originalDataType;
-    options.dataTypes = options.dataType && [options.dataType] || ['*'];
+    var abort = false
+    options.dataType = originalOptions.originalDataType
+    options.dataTypes = options.dataType && [options.dataType] || ['*']
     return {
       send: function(headers, complete) {
-        if (abort) return;
-        me.latest = options;
-        me.calls.set(options.type, options.url, options);
-        var handler = me.handlers.get(options.type, options.url);
+        if (abort) return
+        me.latest = options
+        me.calls.set(options.type, options.url, options)
+        var handler = me.handlers.get(options.type, options.url)
         if (!!handler) {
-          var responses = { text:toText(handler.response) };
-          //, xml:jQuery.parseXML(handler.response) };
+          var responses = { text:toText(handler.response) }
+          //, xml:jQuery.parseXML(handler.response) }
           function done() {
-            complete(handler.statusCode, handler.statusText, responses, '');
+            complete(handler.statusCode, handler.statusText, responses, '')
           }
           if (handler.async && options.async) {
-            setTimeout(done, 0);
+            setTimeout(done, 0)
           } else {
-            done();
+            done()
           }
         } else {
-          warn('No mock ajax registered for '+options.type+' '+options.url);
+          warn('No mock ajax registered for '+options.type+' '+options.url)
         }
       },
-      abort: function() { abort = true; }
-    };
+      abort: function() { abort = true }
+    }
   }
 
   function toParams(data) {
     if ($.isFunction(data)) {
-      data = data();
+      data = data()
     }
     if ($.isArray(data) || $.isPlainObject(data)) {
-      return jQuery.param(data, jQuery.ajaxSettings.traditional);
+      return jQuery.param(data, jQuery.ajaxSettings.traditional)
     } else {
-      return data;
+      return data
     }
   }
 
   function toText(resp) {
     if ($.isFunction(resp)) {
-      resp = resp();
+      resp = resp()
     }
     if ($.isArray(resp) || $.isPlainObject(resp)) {
-      return JSON.stringify(resp);
+      return JSON.stringify(resp)
     } else {
-      return resp;
+      return resp
     }
   }
 
   function wrap(opts) {
     var allOpts = jQuery.ajaxSetup({}, opts)
-    opts.originalDataType = allOpts.dataType;
-    opts.dataType = 'mock';
-    return me.ajax(opts);
+    opts.originalDataType = allOpts.dataType
+    opts.dataType = 'mock'
+    return me.ajax(opts)
   }
 
   function _register(method, url, data) {
-    me.handlers.set(method, url, data);
-    return this;
+    me.handlers.set(method, url, data)
+    return this
   }
   this.register = function(method, url, response) {
-    if (!url) { response = method; method = url = '*'; }
-    var data = { statusCode:200, statusText:'success', response:response };
-    return _register(method, url, data);
+    if (!url) { response = method; method = url = '*' }
+    var data = { statusCode:200, statusText:'success', response:response }
+    return _register(method, url, data)
   }
   this.registerAsync = function(method, url, response) {
-    if (!url) { response = method; method = url = '*'; }
-    var data = { statusCode:200, statusText:'success', response:response, async:true };
-    return _register(method, url, data);
+    if (!url) { response = method; method = url = '*' }
+    var data = { statusCode:200, statusText:'success', response:response, async:true }
+    return _register(method, url, data)
   }
   this.registerError = function(method, url, statusCode, statusText, response) {
-    if (!statusText) { response = statusCode; statusText = url; statusCode = method; method = url = '*'; }
-    var data = { statusCode:statusCode, statusText:statusText, response:response };
-    return _register(method, url, data);
+    if (!statusText) { response = statusCode; statusText = url; statusCode = method; method = url = '*' }
+    var data = { statusCode:statusCode, statusText:statusText, response:response }
+    return _register(method, url, data)
   }
 
   function warn(s) {
-    jasmine.log('smoax: '+s);
+    jasmine.log('smoax: '+s)
   }
 
   this.matchers = {
@@ -100,77 +100,77 @@ function Smoax() {
       this.message = function() { return [
           "Expected ajax to have been invoked.",
           "Expected ajax not to have been invoked."
-      ]; };
-      return me.latest !== undefined;
+      ] }
+      return me.latest !== undefined
     },
     latestInvocationToHaveBeen: function(method, url, data) {
-      return match(this, me.latest, method, url, data);
+      return match(this, me.latest, method, url, data)
     },
     toHaveBeenInvokedWith: function(method, url, data) {
-      var call = me.calls.get(method, url);
-      return match(this, call, method, url, data);
+      var call = me.calls.get(method, url)
+      return match(this, call, method, url, data)
     }
   }
 
   function requestErrorMessages(method, url) {
     function calls2string() {
-      var s = '';
-      for (var key in me.calls.map) { s == '' ? s = key : s = s + ', ' + key; };
-      return s;
+      var s = ''
+      for (var key in me.calls.map) { s == '' ? s = key : s = s + ', ' + key }
+      return s
     }
     return function() {
-      var desc = key(method, url);
+      var desc = key(method, url)
       var details = me.calls.count == 0
         && 'There have been no ajax calls.'
-        || 'There have been calls to: '+calls2string();
+        || 'There have been calls to: '+calls2string()
       return [
         "Expected "+desc+" to have been invoked. "+details,
         "Expected "+desc+" not to have been invoked. "+details
-    ]; };
+    ] }
   }
   function dataErrorMessages(method, url, actualData, expectedData) {
-    var desc = key(method, url);
+    var desc = key(method, url)
     return function() { return [
       "Expected "+desc+" to have been invoked with "+jasmine.pp(expectedData)+" but was invoked with "+jasmine.pp(actualData),
       "Expected "+desc+" not to have been invoked with "+jasmine.pp(expectedData)
-    ]; };
+    ] }
   }
 
   function match(that, opts, method, url, data) {
     var requestMatch =  !!opts && !!opts.type
       && method.toUpperCase() == opts.type.toUpperCase()
-      && url == opts.url.replace(/\?_=\d+/, '');
-   var expectedData = toParams(data);
-   var dataMatch = !!opts && expectedData == opts.data;
+      && url == opts.url.replace(/\?_=\d+/, '')
+   var expectedData = toParams(data)
+   var dataMatch = !!opts && expectedData == opts.data
    if (!requestMatch) {
-     that.message = requestErrorMessages(method, url);
+     that.message = requestErrorMessages(method, url)
    } else if (!dataMatch) {
-     that.message = dataErrorMessages(method, url, opts.data, expectedData);
+     that.message = dataErrorMessages(method, url, opts.data, expectedData)
    }
-   return requestMatch && dataMatch;
+   return requestMatch && dataMatch
   }
 
   function key(method, url) {
-    return method.toUpperCase()+' '+url.replace(/\?_=\d+/, '');
+    return method.toUpperCase()+' '+url.replace(/\?_=\d+/, '')
   }
 
   function AjaxMap() {
-    this.count = 0;
-    this.map = {};
+    this.count = 0
+    this.map = {}
 
     this.get = function(method, url) {
-      return this.map[key(method, url)] || this.map['* *'];
-    };
-    this.set = function(method, url, data) {
-      var k = key(method, url);
-      if (!this.map[k]) {
-        this.count++;
-      }
-      this.map[k] = data;
+      return this.map[key(method, url)] || this.map['* *']
     }
-    return this;
+    this.set = function(method, url, data) {
+      var k = key(method, url)
+      if (!this.map[k]) {
+        this.count++
+      }
+      this.map[k] = data
+    }
+    return this
   }
 
-  return this;
-};
+  return this
+}
 
--- spec/jasmine-test.js
+++ spec/jasmine-test.js
@@ -1,131 +1,131 @@
 describe('smoax', function() {
   beforeEach(function() {
-    this.addMatchers(smoax.setup());
-  });
+    this.addMatchers(smoax.setup())
+  })
   
   it('gets initialised', function() {
-    expect(smoax).not.toHaveBeenInvoked();
-    expect(smoax).not.toHaveBeenInvokedWith('post', '/url', {data:false});
-  });
+    expect(smoax).not.toHaveBeenInvoked()
+    expect(smoax).not.toHaveBeenInvokedWith('post', '/url', {data:false})
+  })
   
   it('handles $.get', function() {
-    smoax.register('get', '/url', { data: "yeah" });
-    var gotSyncCall = false;
+    smoax.register('get', '/url', { data: "yeah" })
+    var gotSyncCall = false
     $.get('/url', function(resp) {
-      gotSyncCall = true;
-      expect(resp.data).toEqual('yeah');
-    }, 'json');
-    expect(gotSyncCall).toEqual(true);
-    expect(smoax).toHaveBeenInvoked();
-    expect(smoax).toHaveBeenInvokedWith('get', '/url');
-    expect(smoax).not.toHaveBeenInvokedWith('get', '/urlX');
-  });
+      gotSyncCall = true
+      expect(resp.data).toEqual('yeah')
+    }, 'json')
+    expect(gotSyncCall).toEqual(true)
+    expect(smoax).toHaveBeenInvoked()
+    expect(smoax).toHaveBeenInvokedWith('get', '/url')
+    expect(smoax).not.toHaveBeenInvokedWith('get', '/urlX')
+  })
   
   it('handles $.post', function() {
-    smoax.register('post', '/url', { data: "yeah" });
-    var gotSyncCall = false;
+    smoax.register('post', '/url', { data: "yeah" })
+    var gotSyncCall = false
     $.post('/url', { request: true }, function(resp) {
-      gotSyncCall = true;
-      expect(resp.data).toEqual('yeah');
-    }, 'json');
-    expect(gotSyncCall).toEqual(true);
-    expect(smoax).toHaveBeenInvokedWith('post', '/url', { request:true });
-    expect(smoax).not.toHaveBeenInvokedWith('post', '/url', { request:false });
-  });
+      gotSyncCall = true
+      expect(resp.data).toEqual('yeah')
+    }, 'json')
+    expect(gotSyncCall).toEqual(true)
+    expect(smoax).toHaveBeenInvokedWith('post', '/url', { request:true })
+    expect(smoax).not.toHaveBeenInvokedWith('post', '/url', { request:false })
+  })
   
   
   it('handles error callback', function() {
-    smoax.registerError('get', '/url', 404, 'notfound');
-    var successGotCalled = false;
-    var errorGotCalled = false;
+    smoax.registerError('get', '/url', 404, 'notfound')
+    var successGotCalled = false
+    var errorGotCalled = false
     $.ajax({ type: 'get', url: '/url', 
-      success: function(data) { successGotCalled = true; },
+      success: function(data) { successGotCalled = true },
       error: function(xhr, statusText, errorThrown) { 
-        errorGotCalled = true;
-        expect(xhr.status).toEqual(404);
-        expect(statusText).toEqual('error');
-        expect(errorThrown).toEqual('notfound');
+        errorGotCalled = true
+        expect(xhr.status).toEqual(404)
+        expect(statusText).toEqual('error')
+        expect(errorThrown).toEqual('notfound')
       }
-    });
-    expect(successGotCalled).toEqual(false);    
-    expect(errorGotCalled).toEqual(true);
-    expect(smoax).toHaveBeenInvoked();
-    expect(smoax).toHaveBeenInvokedWith('get', '/url');
-  });
+    })
+    expect(successGotCalled).toEqual(false)
+    expect(errorGotCalled).toEqual(true)
+    expect(smoax).toHaveBeenInvoked()
+    expect(smoax).toHaveBeenInvokedWith('get', '/url')
+  })
 
   it('handles multiple deferred success callbacks', function() {
-    smoax.register('get', '/url', { data: "yeah" });
-    var calls = 0;
+    smoax.register('get', '/url', { data: "yeah" })
+    var calls = 0
     $.get('/url')
-      .success(function() { calls++; })
-      .success(function() { calls++; })      
-    expect(calls).toEqual(2);
-  });
+      .success(function() { calls++ })
+      .success(function() { calls++ })
+    expect(calls).toEqual(2)
+  })
 
   it('handles multiple deferred error callbacks', function() {
-    smoax.registerError('get', '/url', 404, 'notfound');
-    var calls = 0;
+    smoax.registerError('get', '/url', 404, 'notfound')
+    var calls = 0
     $.get('/url')
-      .error(function() { calls++; })
-      .error(function() { calls++; })      
-    expect(calls).toEqual(2);
-  });
+      .error(function() { calls++ })
+      .error(function() { calls++ })
+    expect(calls).toEqual(2)
+  })
 
   it('registers calls and call count', function() {
-    smoax.register('get', '/url', { data: "yeah" });
-    expect(smoax.calls.map).toEqual({});
-    $.get('/url', function() {});
-    expect(smoax.calls.map['GET /url']).toBeDefined();
-    expect(smoax.calls.count).toEqual(1);
-  });
+    smoax.register('get', '/url', { data: "yeah" })
+    expect(smoax.calls.map).toEqual({})
+    $.get('/url', function() {})
+    expect(smoax.calls.map['GET /url']).toBeDefined()
+    expect(smoax.calls.count).toEqual(1)
+  })
   
   it('can also play async', function() {
-    smoax.registerAsync('get', '/url', { data: "yeah" });
-    var called = false;
+    smoax.registerAsync('get', '/url', { data: "yeah" })
+    var called = false
     $.get('/url', function(resp) {
-      called = true;
-    });
-    expect(called).toEqual(false);
-    waitsFor(function() { return called; }, 1000);
-  });
+      called = true
+    })
+    expect(called).toEqual(false)
+    waitsFor(function() { return called }, 1000)
+  })
   
   it('exposes real ajax', function() {
     if (navigator.userAgent.toLowerCase().indexOf('chrome') > -1 && document.location.protocol == 'file:') {
-      jasmine.log('This test fails with google-chrome if not started with --allow-file-access-from-files');
+      jasmine.log('This test fails with google-chrome if not started with --allow-file-access-from-files')
     }
-    var response = smoax.ajax({type:'get', url:'../spec/jasmine-test.js', async:false});
-    expect(response.responseText).toMatch(/^describe\('smoax/);
-  });
+    var response = smoax.ajax({type:'get', url:'../spec/jasmine-test.js', async:false})
+    expect(response.responseText).toMatch(/^describe\('smoax/)
+  })
 
   it('can use a string response body', function() {
-    smoax.register('get', '/url', 'foo');
-    $.get('/url').success(function(data) { expect(data).toEqual('foo'); });
-    expect(smoax.calls.count).toEqual(1);
-  });
+    smoax.register('get', '/url', 'foo')
+    $.get('/url').success(function(data) { expect(data).toEqual('foo') })
+    expect(smoax.calls.count).toEqual(1)
+  })
 
   it('can use a function response body', function() {
-    smoax.register('get', '/url', function() { return 'foo'; });
-    $.get('/url').success(function(data) { expect(data).toEqual('foo'); });
-    expect(smoax.calls.count).toEqual(1);
-  });
+    smoax.register('get', '/url', function() { return 'foo' })
+    $.get('/url').success(function(data) { expect(data).toEqual('foo') })
+    expect(smoax.calls.count).toEqual(1)
+  })
 
   it('uses readable error messages when method and url doesnt match expected', function() {
-    $.get('/first');
-    $.post('/second');
-    var obj = {};
-    var result = smoax.matchers.toHaveBeenInvokedWith.call(obj, 'get', '/second');
-    expect(result).toEqual(false);
-    expect(obj.message()[0]).toEqual('Expected GET /second to have been invoked. There have been calls to: GET /first, POST /second');
-    expect(obj.message()[1]).toEqual('Expected GET /second not to have been invoked. There have been calls to: GET /first, POST /second');
-  });
+    $.get('/first')
+    $.post('/second')
+    var obj = {}
+    var result = smoax.matchers.toHaveBeenInvokedWith.call(obj, 'get', '/second')
+    expect(result).toEqual(false)
+    expect(obj.message()[0]).toEqual('Expected GET /second to have been invoked. There have been calls to: GET /first, POST /second')
+    expect(obj.message()[1]).toEqual('Expected GET /second not to have been invoked. There have been calls to: GET /first, POST /second')
+  })
 
   it('uses readable error messages when data doesnt match expected', function() {
-    $.post('/second', { data:'ok' });
-    var obj = {};
-    var result = smoax.matchers.toHaveBeenInvokedWith.call(obj, 'post', '/second', { data:'fail' });
-    expect(result).toEqual(false);
-    expect(obj.message()[0]).toEqual("Expected POST /second to have been invoked with 'data=fail' but was invoked with 'data=ok'");
-    expect(obj.message()[1]).toEqual("Expected POST /second not to have been invoked with 'data=fail'");
-  });
+    $.post('/second', { data:'ok' })
+    var obj = {}
+    var result = smoax.matchers.toHaveBeenInvokedWith.call(obj, 'post', '/second', { data:'fail' })
+    expect(result).toEqual(false)
+    expect(obj.message()[0]).toEqual("Expected POST /second to have been invoked with 'data=fail' but was invoked with 'data=ok'")
+    expect(obj.message()[1]).toEqual("Expected POST /second not to have been invoked with 'data=fail'")
+  })
 
-});
+})
